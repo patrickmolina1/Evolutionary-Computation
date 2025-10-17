72ec960ac12ed155d97e3782547c3980420fdbc0
ChihabEddine Zitouni <cezitouni@gmail.com>
2025-10-17 19:00:17 +0200

    first try for greedy regret heuristics
    Co-authored-by: Patrick Molina <patrickmolina102@hotmail.com>

--- src/GreedyRegretHeuristics/GreedyRegretHeuristicsExperimentRunner.java
+++ src/GreedyRegretHeuristics/GreedyRegretHeuristicsExperimentRunner.java
@@ -17,16 +17,67 @@ public class GreedyRegretHeuristicsExperimentRunner extends ExperimentRunner imp
 
     @Override
     public List<ExperimentResult> runExperiments(Instance instance, int numIterations) {
+        List<ExperimentResult> results = new ArrayList<>();
 
-        return null;
+        // Test each method
+        results.add(testMethod(instance, "greedy2RegretNearest", numIterations));
+        results.add(testMethod(instance, "greedyWeightedRegret", numIterations));
+
+
+        return results;
     }
 
     @Override
     public ExperimentResult testMethod(Instance instance, String methodName, int numIterations) {
+        List<Solution> solutions = new ArrayList<>();
 
-        return null;
+
+        for (int i = 0; i < numIterations; i++) {
+            Solution solution = null;
+
+            switch (methodName) {
+                case "greedy2RegretNearest":
+                    solution = solver.greedy2RegretNearest(instance, instance.nodes.get(i));
+                    break;
+                case "greedyWeightedRegret":
+                    solution = solver.greedyWeightedRegret(instance, instance.nodes.get(i), 0.5, 0.5);
+                    break;
+            }
+
+            if (solution != null) {
+                solutions.add(solution);
+            }
+        }
+
+        // Calculate statistics and find best solution
+        int minCost = Integer.MAX_VALUE;
+        int maxCost = 0;
+        int bestSolutionId = 0;
+
+        for (int i = 0; i < solutions.size(); i++) {
+            int cost = solutions.get(i).totalCost;
+            if (cost < minCost) {
+                minCost = cost;
+                bestSolutionId = i + 1; // 1-based index
+            }
+            if (cost > maxCost) {
+                maxCost = cost;
+            }
+        }
+
+        double avgCost = solutions.stream().mapToInt(s -> s.totalCost).average().orElse(0.0);
+
+        return new ExperimentResult(
+                instance.name,
+                methodName,
+                minCost,
+                maxCost,
+                avgCost,
+                solutions.size(),
+                bestSolutionId,
+                solutions
+        );
     }
-
 
 
 }
