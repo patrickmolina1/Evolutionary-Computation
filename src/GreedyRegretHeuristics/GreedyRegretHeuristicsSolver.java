101a8a20cccb2ca21f232c25624463552b015d45
ChihabEddine Zitouni <cezitouni@gmail.com>
2025-10-17 17:53:34 +0200

    GreedyRegretHeuristics init commit

---
+++ src/GreedyRegretHeuristics/GreedyRegretHeuristicsSolver.java
@@ -0,0 +1,23 @@
+package GreedyRegretHeuristics;
+
+import Utilities.Instance;
+import Utilities.Node;
+import Utilities.Solution;
+import Utilities.Solver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
+public class GreedyRegretHeuristicsSolver extends Solver {
+
+    public GreedyRegretHeuristicsSolver(){
+
+    }
+
+
+
+
+}
